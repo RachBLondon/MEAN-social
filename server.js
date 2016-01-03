7ed0bde264384a55ffb6e681e6944e14ada6e72a
Rachel Black <rachel.black1984@googlemail.com>
2016-01-03 11:26:32 +0000

    added controllers/static.js to route static files

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var Post = require('./models/post');
+// var Post = require('./models/post');
 
 var app = express();
 app.use(bodyParser.json());
 
-app.use(require('./controllers/api/posts'));
-
-app.get('/',function(req, res){
-  res.sendfile('layouts/posts.html');
-})
+app.use(require('./controllers/api/posts.js'));
+app.use(require('./controllers/static.js'));
 
 app.listen(3000, function(){
   console.log('Server listening on', 3000);
