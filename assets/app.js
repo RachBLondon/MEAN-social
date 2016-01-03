5d723e7919bbdb0a8d14a58265f3cc185b1010ad
Rachel Black <rachel.black1984@googlemail.com>
2016-01-03 14:30:39 +0000

    modularised angular

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,27 @@
 var app = angular.module('app',[]);
 
-app.controller('PostsCtrl', function($scope, $http){
+app.controller('PostsCtrl', function($scope, PostsSvc){
     $scope.addPost = function(){
       if($scope.postBody){
-        $http.post('/api/posts',{
-          username : "Rachy B",
-          body     : $scope.postBody
+        PostsSvc.create({
+            username : "Rachy B",
+            body     : $scope.postBody
         }).success(function(post){
           $scope.posts.unshift(post);
           $scope.postBody = null;
         })
+      }
     }
-  }
-
-  $http.get('http://localhost:3000/api/posts')
-  .success(function(posts){
-    $scope.posts = posts
+  PostsSvc.fetch().success(function(posts){
+    $scope.posts = posts;
   })
+})
+
+app.service('PostsSvc',function($http){
+  this.fetch = function(){
+    return $http.get('/api/posts');
+  }
+  this.create = function(post){
+    return $http.post('/api/posts', post);
+  }
 })
