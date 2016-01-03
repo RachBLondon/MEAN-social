6c8e648360732620ddbc0ef017e16671820f7d03
Rachel Black <rachel.black1984@googlemail.com>
2016-01-03 13:54:56 +0000

    fixed sendFile bug in controllers/static.js

--- controllers/static.js
+++ controllers/static.js
@@ -1,7 +1,22 @@
-var router = require('express').Router();
+var express = require('express');
+var router  = express.Router();
+var path = require('path');
 
 router.get('/', function(req, res){
-  res.sendfile('layouts/posts.html');
+  res.sendFile(path.resolve('layouts/posts.html'));
 });
 
+
+
+
+
+
+
 module.exports = router;
+
+// old code does not work
+// var router = require('express').Router();
+//
+// router.get('/', function(req, res){
+//   res.sendfile('layouts/posts.html');
+// });
