f5fe36fc3fac74704dad8ef50fad6a2d8075afa9
Rachel Black <rachel.black1984@googlemail.com>
2016-01-03 18:55:15 +0000

    gulp stylus added to handle css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
-var uglify = require('gulp-uglify');
-var ngAnnotate = require('gulp-ng-annotate');
+var gulp        = require('gulp');
+var concat      = require('gulp-concat');
+var sourcemaps  = require('gulp-sourcemaps');
+var uglify      = require('gulp-uglify');
+var ngAnnotate  = require('gulp-ng-annotate');
+var fs          = require('fs');
 
 
 gulp.task('js', function(){
@@ -17,4 +18,13 @@ gulp.task('js', function(){
 
 gulp.task('watch:js', ['js'], function(){
   gulp.watch('ng/**/*.js', ['js'])
+})
+
+gulp.task('watch:css', function(){
+  gulp.watch('css/**/*.styl', ['css'])
+})
+
+
+fs.readdirSync(__dirname + '/gulp').forEach(function (task){
+  require('./gulp/' + task)
 })
